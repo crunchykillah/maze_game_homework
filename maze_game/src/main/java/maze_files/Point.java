c525b7167936eb95dd57c78518d98d8ab2adf394
Adeeliore <116266434+Adeeliore@users.noreply.github.com>
2023-10-05 22:53:39 +0300

    commit: added Point class to save coordinates

---
+++ maze_game/src/main/java/maze_files/Point.java
@@ -0,0 +1,10 @@
+package maze_files;
+//Класс точки, сделан для удобства поиска в ширину
+public class Point {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
